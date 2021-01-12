20641d909a82fd7f404997087c338373dc94b217
Subhash Kumar <31248887+imsubh@users.noreply.github.com>
2021-01-12 11:10:04 +0530

    Add files via upload

---
+++ python/Activity5.py
@@ -0,0 +1,5 @@
+number = int(input("Input a number pleaseeeee: "))
+
+
+for i in range(1,20):
+   print(number, ' x ', i, ' = ', number*i)
---
+++ python/Activity6.py
@@ -0,0 +1,5 @@
+
+# n= int(input("enter a number please : "))
+
+for i in range(0,10):
+    print(f" hello  {str(i)*i}")
