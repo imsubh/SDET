e82789d57bbc264a94a8a8fe8d92a7e24ed1e062
Subhash Kumar <31248887+imsubh@users.noreply.github.com>
2021-01-15 09:34:07 +0530

    Add files via upload

---
+++ python/list.py
@@ -0,0 +1,14 @@
+	
+# example_list = ["apple", "banana", "cherry"]
+# example_list[2] = "blackcurrant"
+# print(example_list) 
+
+
+from datetime import datetime
+
+x = datetime.now()
+
+print(x) 
+
+
+# Output: 2020-05-01 12:11:17.902820
